a729fa4145e13674f2ce344af65bc9a4ca1b29e9
Abdullah-crns <u2023037@giki.edu.pk>
2025-02-19 12:22:38 +0500

    updated

--- LAB-3/task3.asm
+++ LAB-3/task3.asm
@@ -1,14 +1,16 @@
 .data
-    value_1 : .word 0x20 @ Define the first value as 0x20
-    value_2 : .word 0x21 @ Define the second value as 0x21
-    result  : .word 0x0  @ Reserve space for the result of the addition
+    value_1 : .word 0x20 # Define the first value as 0x20
+    value_2 : .word 0x21 # Define the second value as 0x21
+    result  : .word 0x0  # Reserve space for the result of the addition
 
 .text
-    lw t0,value_1       @ Load the value of value_1 into register t0
-    lw t1,value_2       @ Load the value of value_2 into register t1
+    la t0,value_1       # Load the address of value_1 into register t0
+    la t1,value_2       # Load the address of value_2 into register t1
+    lw t1,0(t0)         # Load the value of value_1 into register t0
+    lw t2,0(t1)         # Load the value of value_2 into register t1
 
-    add s0,t0,t1        @ Add the values in t0 and t1, store the result in s0
+    add s0,t0,t1        # Add the values in t0 and t1, store the result in s0
 
-    sw s0,result,t3     @ Store the result from s0 into the memory location of result
+    sw s0,result,t3     # Store the result from s0 into the memory location of result
 
-    lw t2,result        @ Load the result from memory into register t2
+    lw t2,result        # Load the result from memory into register t2
