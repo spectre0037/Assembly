6aad466ca83d5f1ef119e664878ed7816cab2a8d
Abdullah-crns <u2023037@giki.edu.pk>
2025-02-19 12:27:52 +0500

    updated

--- LAB-3/task3.asm
+++ LAB-3/task3.asm
@@ -4,8 +4,8 @@
     result  : .word 0x0  # Reserve space for the result of the addition
 
 .text
-    la t0,value_1       # Load the address of value_1 into register t0
-    la t1,value_2       # Load the address of value_2 into register t1
+    la t0,value_1       # load the address of value_1 into register t0
+    la t1,value_2       # load the address of value_2 into register t1
     lw t1,0(t0)         # Load the value of value_1 into register t0
     lw t2,0(t1)         # Load the value of value_2 into register t1
 
