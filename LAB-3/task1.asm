9c63ff4920f29c62f446fc491446dc9c03eecb6b
Abdullah-crns <u2023037@giki.edu.pk>
2025-02-19 12:25:50 +0500

    updated

--- LAB-3/task1.asm
+++ LAB-3/task1.asm
@@ -1,5 +1,5 @@
 # task 1
-# assigning the value of 0x20 to s0
+# load the value of 0x20 to s0
 LUI s0,0x2023
-# assigning the value of 0x21 to s1
+# load the value of 0x21 to s1 and store the result in s0
 addi s0,s1,037
