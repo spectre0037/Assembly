cf7b4098505224d584ce4897dd83b4fc5d496489
Abdullah-crns <u2023037@giki.edu.pk>
2025-02-26 12:00:15 +0500

    updated

--- LAB-4/Task4.asm
+++ LAB-4/Task4.asm
@@ -1,32 +1,32 @@
 # part a
-LI s0 , 8
-LI s1 , 2
-LI s3 , 4
-LI s4 , 5
+LI s0 , 8          # Load immediate value 8 into register s0
+LI s1 , 2          # Load immediate value 2 into register s1
+LI s3 , 4          # Load immediate value 4 into register s3
+LI s4 , 5          # Load immediate value 5 into register s4
 
-BNE s0,s1,l1
+BNE s0,s1,l1       # Branch to label l1 if s0 is not equal to s1
 
-equal:
-ADD s2 , s3,s4
-li a0 ,0
+equal:             # Label equal
+ADD s2 , s3,s4     # Add values in s3 and s4, store result in s2
+li a0 ,0           # Load immediate value 0 into register a0
 
-l1:
-SUB s0,s4,s1
+l1:                # Label l1
+SUB s0,s4,s1       # Subtract value in s1 from value in s4, store result in s0
 
 # part b
 
-LI s0 , 8
-LI s1 , 2
-LI s3 , 4
-LI s4 , 5
+LI s0 , 8          # Load immediate value 8 into register s0
+LI s1 , 2          # Load immediate value 2 into register s1
+LI s3 , 4          # Load immediate value 4 into register s3
+LI s4 , 5          # Load immediate value 5 into register s4
 
-BNE s0,s1,l1
-j l2
+BNE s0,s1,l1       # Branch to label l1 if s0 is not equal to s1
+j l2               # Jump to label l2
 
-equal:
-ADD s2 , s3,s4
+equal:             # Label equal
+ADD s2 , s3,s4     # Add values in s3 and s4, store result in s2
 
-l1:
-SUB s0,s4,s1
+l1:                # Label l1
+SUB s0,s4,s1       # Subtract value in s1 from value in s4, store result in s0
 
-l2:
+l2:                # Label l2
